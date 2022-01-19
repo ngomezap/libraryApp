c882605b9ceb2316c47aa7db14bd1b93f53fa67e
ngomezap <igomez.ap@gmail.com>
2022-01-19 18:53:47 +0100

    Create basic library with delete and read opt

--- script.js
+++ script.js
@@ -31,32 +31,92 @@ function addBookLibrary(){
 }
 
 function addToTable(book){
+    //Create 'div' where book data will be stored
     const divBook = document.createElement('div');
-    const button = document.createElement('button');
-    button.innerText = 'Delete';
-    button.setAttribute('data-index', myLibrary.map(e => e.title).indexOf(book.title));
     container.appendChild(divBook);
+    //Loop all keys in book
     for(key in book){
         if(key === 'info'){
             continue;
+        }else if(key === 'read'){
+            if(book[key]){
+                const div = document.createElement('div');
+                div.innerText = 'Read';
+                div.setAttribute('class', 'read');
+                divBook.appendChild(div);
+            }else{
+                const div = document.createElement('div');
+                div.innerText = 'Not read';
+                div.setAttribute('class', 'notRead');
+                divBook.appendChild(div);
+            }
         }else{
             const div = document.createElement('div');
             div.innerText = book[key];
             div.setAttribute('data-index', myLibrary.map(e => e.title).indexOf(book.title));
             divBook.appendChild(div);
+            if(key === 'pages'){
+                div.innerText += ' pp';
+            }
         }
     }
-    divBook.appendChild(button);
-    button.addEventListener('click', deleteBook)
+    //Add delete button for every book
+    deleteColumn(book);
+    //Add read button for every book
+    readColumn(book);
 }
 
 function deleteBook(e){
-    const bookDivs = Array.from(container.childNodes).splice(3);
+    const bookDivs = Array.from(container.childNodes);
     bookDivs.forEach(function(book){
-        if(e.target.getAttribute('data-index') === book.firstChild.getAttribute('data-index')){
+        if(e.currentTarget.getAttribute('data-index') === book.firstChild.getAttribute('data-index')){
             book.remove();
         }
     })
+}
+
+function readBook(e){
+    const bookDivs = Array.from(container.childNodes);
+    
+    bookDivs.forEach(function(book){
+        let index = book.firstChild.getAttribute('data-index');
+        if(e.currentTarget.getAttribute('data-index') === index){
+            if(myLibrary[index].read){
+                myLibrary[index].read = false;
+                book.childNodes[3].innerText = 'Not read';
+                book.childNodes[3].setAttribute('class', 'notRead');
+            }else{
+                myLibrary[index].read = true;
+                book.childNodes[3].innerText = 'Read';
+                book.childNodes[3].setAttribute('class', 'read');
+            }
+        }
+    })
+}
+
+function deleteColumn(book){
+    deleteButton = document.createElement('button');
+    deleteButton.setAttribute('data-index', myLibrary.map(e => e.title).indexOf(book.title));
+    container.lastChild.appendChild(deleteButton);
+    
+    //Add icon to delete button
+    crossSymbol = document.createElement('img');
+    crossSymbol.setAttribute('src', './images/symbolX.svg');
+    deleteButton.appendChild(crossSymbol);
+
+    deleteButton.addEventListener('click', deleteBook)
+}
+
+function readColumn(book){
+    readButton = document.createElement('button');
+    readButton.setAttribute('data-index', myLibrary.map(e => e.title).indexOf(book.title));
+    
+    //Add icon to read button
+    eyeSymbol = document.createElement('img');
+    eyeSymbol.setAttribute('src', './images/symbolEye.png');
+    readButton.appendChild(eyeSymbol);
+    container.lastChild.appendChild(readButton);
+    readButton.addEventListener('click', readBook);
 }
 
 const container = document.getElementById('books');
@@ -64,11 +124,13 @@ const newBook = document.getElementById('newBook');
 
 let myLibrary = [];
 
-myLibrary.push(new Book('The Hobbit', 'JRR Tolkien', 295, false));
-myLibrary.push(new Book('The Identity', 'Milan Kundera', 180, true));
+const laPeste = new Book('La Peste', 'Albert Camus', 359, true);
+const elExtranjero = new Book('El Extranjero', 'Albert Camus', 201, false);
+
+myLibrary.push(laPeste, elExtranjero);
 
 myLibrary.forEach(function(book){
     addToTable(book);
-});
+})
 
 newBook.addEventListener('click', addBookLibrary)
